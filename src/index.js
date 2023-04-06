ebd4cda69b4296b33a6fb1d20e136be433406560
Xylon Funes <xyfullns@gmail.com>
2023-04-05 22:52:28 -0400

    To-Do-Update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleToDo(e.target.newTask.value);
+    form.reset();
+  });
 });
+
+function handleToDo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.textContent = "X";
+  btn.addEventListener("click", handleDelete);
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
